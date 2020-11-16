32580ca65fd0e7d0ccd3f6ba64960cc72b2c641d
Administrator <Administrator@pjx>
2020-11-16 08:15:34 +0800

    封装

---
+++ src/com/pjxpro/fengzhuang/Animal.java
@@ -0,0 +1,38 @@
+package com.pjxpro.fengzhuang;
+
+public class Animal {
+      private String name;
+      private int legs;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getLegs() {
+		return legs;
+	}
+	public void setLegs(int legs) {
+		this.legs = legs;
+	}
+	public Animal(String name, int legs) {
+		super();
+		this.name = name;
+		this.legs = legs;
+	}
+	public Animal() {
+		this.name = "AAA";
+		this.legs = 4;
+	}
+	public void move(){
+     System.out.print(this.name+"Moving");
+	}
+     public void move(int n){
+    	 while(n--!=0){
+    		 System.out.print(this.name+"Moving");
+    	 }
+		
+	}
+	
+      
+}
